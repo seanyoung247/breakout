08866da0ba9a49235ff7aed93ea85d138a57ea7b
seanyoung247 <seanyoung247@gmail.com>
2021-06-11 00:49:16 +0100

    feat: initial game entry point code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Get the game canvas
   const gameDiv = document.getElementById('gameWrapper');
   const canvas = document.getElementById('gameCanvas');
-  const ctx = canvas.getContext("2d");
   // Make the canvas fill the viewport
-  ctx.canvas.width = gameDiv.clientWidth;
-  ctx.canvas.height = gameDiv.clientHeight;
+  canvas.width = gameDiv.clientWidth;
+  canvas.height = gameDiv.clientHeight;
   // Instantiate the game object
-  game = new Game();
+  game = new Game(canvas, gameDiv);
 });
