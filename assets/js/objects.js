f6e9e2719c56aeb8eff11120a89f758bd8ca6d19
seanyoung247 <seanyoung247@gmail.com>
2021-06-12 11:12:21 +0100

    feat: improves paddle movement code

--- assets/js/objects.js
+++ assets/js/objects.js
@@ -61,8 +61,23 @@ class Paddle extends Block {
   set speed(val) {
     this._speed = val;
   }
-  move(timeDelta, direction) {
-    this._box.x = this._box.x + ((this._speed / timeDelta) * direction);
+  _move(bounds, timeDelta, direction) {
+    let newX = this._box.x + ((this._speed / timeDelta) * direction);
+    // Clamp to the left side of the screen
+    if (newX < bounds.x) {
+      newX = bounds.x;
+    }
+    // Clamp to the right side of the screen
+    if (newX + this._box.width > bounds.x + bounds.width) {
+      newX = bounds.x - this._box.width;
+    }
+    this._box.x = newX;
+  }
+  moveLeft(bounds, timeDelta) {
+    this._move(bounds, timeDelta, -1);
+  }
+  moveRight(bounds, timeDelta) {
+    this._move(bounds, timeDelta, 1);
   }
 }
 
