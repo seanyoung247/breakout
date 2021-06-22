42170c035ce9e96b03f63fd14652640b91075b95
seanyoung247 <seanyoung247@gmail.com>
2021-06-22 19:35:34 +0100

    test: adds collision detection tests

--- spec/objectsSpec.js
+++ spec/objectsSpec.js
@@ -23,10 +23,28 @@ describe("GameObject", function() {
   });
 
   describe("collision", function() {
-
+    it("detects collision", function() {
+      let object2 = new TestGameObject(null, new BoundingBox(5,5,10,10));
+      expect(object.collision(object2)).toBeTrue();
+    });
+    it("detects not collision", function() {
+      let object2 = new TestGameObject(null, new BoundingBox(11,11,10,10));
+      expect(object.collision(object2)).toBeFalse();
+    });
   });
 
   describe("intersects", function() {
-
+    it("detects intersection", function() {
+      let object2 = new TestGameObject(null, new BoundingBox(5,5,10,10));
+      expect(object.intersects(object2)).toBeTruthy();
+    });
+    it("detects non-intersection", function() {
+      let object2 = new TestGameObject(null, new BoundingBox(12,12,10,10));
+      expect(object.intersects(object2)).toBeFalsy();
+    });
+    it("detects axis of intersection", function() {
+      let object2 = new TestGameObject(null, new BoundingBox(9,8,10,10));
+      expect(object.intersects(object2)).toEqual({side: 'x', pos: 12});
+    });
   });
 });
