338ebe8a38779986e70a8e346989f70db4600e3e
seanyoung247 <seanyoung247@gmail.com>
2021-06-22 03:26:05 +0100

    test: adds draw method tests to GameObject spec

--- spec/objectsSpec.js
+++ spec/objectsSpec.js
@@ -2,9 +2,31 @@
  * GameObject tests
  */
 describe("GameObject", function() {
+  class TestGameObject extends GameObject {
+    constructor(game, box) {super(game, box);}
+  }
+
+  let object;
+  beforeEach(function() {
+    object = new TestGameObject(null, new BoundingBox(1,1,10,10));
+  });
+
   it("can't be created", function() {
     expect(() => new GameObject()).toThrowError(
       TypeError, 'Abstract class "GameObject" cannot be instantiated directly');
   });
 
+  describe("draw", function() {
+    it("enforces draw method definition", function() {
+      expect(() => object.draw(null)).toThrowError(TypeError);
+    });
+  });
+
+  describe("collision", function() {
+
+  });
+
+  describe("intersects", function() {
+
+  });
 });
