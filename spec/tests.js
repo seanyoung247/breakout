8009f3ad89804801d9d543328817055e061bc154
seanyoung247 <seanyoung247@gmail.com>
2021-07-11 15:54:11 +0100

    test: adds test.js main test script

---
+++ spec/tests.js
@@ -0,0 +1,16 @@
+
+import { Point2D_Tests, Vector2D_Tests, BoundingBox_Tests } from "./typesSpec.js";
+
+import { AbstractClass_Tests } from "./abstractSpec.js";
+import { GameObject_Tests, Block_Tests, Paddle_Tests, Ball_Tests } from "./objectsSpec.js"
+
+describe("Point2D", Point2D_Tests);
+describe("Vector2D", Vector2D_Tests);
+describe("BoundingBox", BoundingBox_Tests);
+
+describe("AbstractClass", AbstractClass_Tests);
+
+describe("GameObject", GameObject_Tests);
+describe("Block", Block_Tests);
+describe("Paddle", Paddle_Tests);
+describe("Ball", Ball_Tests);
