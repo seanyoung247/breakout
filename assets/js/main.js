4d3c28024565db1b9720a335077a3fe09cad7336
seanyoung247 <seanyoung247@gmail.com>
2021-07-11 13:14:24 +0100

    feat: removes game object global variable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
-var game = null;
 document.addEventListener("DOMContentLoaded", function(event) {
   // Get the game canvas
   const canvas = document.getElementById('gameCanvas');
   // Instantiate the game object
-  game = new Game(canvas);
+  let game = new Game(canvas);
 });
